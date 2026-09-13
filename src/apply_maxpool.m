947b95024179b1057582909bfc16c99c2931b79b
Kyle Nicarry <kylenicarry@Kyles-MacBook-Pro.local>
2026-09-13 18:03:58 -0400

    Implement max pooling and softmax

--- src/apply_maxpool.m
+++ src/apply_maxpool.m
@@ -1,3 +1,26 @@
 function outarray = apply_maxpool(inarray)
     %inarray is 2Nx2MxD and outarray is size NxMxD
+    % extract dimension size
+    twoN = size(inarray, 1);
+    twoM = size(inarray, 2);
+    D = size(inarray, 3);
+
+    % initialize outarray
+    outarray = zeros(twoN/2, twoM/2, D);
+    
+    % iterate over D indices
+    for d = 1:D
+        % iterate over N indices
+        for i = 1:twoN/2
+            % iterate over M indices
+            for j = 1:twoM/2
+
+                % compute subarray block (using Figure 1A as reference)
+                subarray = inarray(2*i-1:2*i, 2*j-1:2*j, d);
+                
+                % populate outarray with maximum value from vectorized form of subarray 
+                outarray(i, j, d) = max(subarray(:));
+            end
+        end
+    end
 end
--- src/apply_softmax.m
+++ src/apply_softmax.m
@@ -1,3 +1,16 @@
 function outarray = apply_softmax(inarray)
     %inarray is 1x1xD and outarray is the same size
+    D = size(inarray, 3); % define dimension size d
+    a = max(inarray(1, 1, 1:D)); % compute alpha using max value over all ks
+    outarray = zeros(1, 1, D);% initialize outarray
+    ksum = 0; % initialize sum of all k values
+
+    % compute sum of shifted exponentials over all k 
+    for k = 1:D
+        ksum = ksum + exp((inarray(1, 1, k) - a));
+    end
+
+    for k = 1:D
+        outarray(1,1,k) = (exp(inarray(1, 1, k) - a))/ksum; 
+    end
 end
