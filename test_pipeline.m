8dfefa751d067e6c1aaadf9255c52b1db5259ac7
KyleNicarry <nicarrykyle@gmail.com>
2026-09-13 23:47:46 -0400

    Create test script

---
+++ test_pipeline.m
@@ -0,0 +1,14 @@
+% script for testing CNN pipeline
+
+% make functions in the 'src' directory callable
+addpath('src');
+
+% load parameters and expected results
+load('Project1DataFiles/debuggingTest.mat');
+load('Project1DataFiles/CNNparameters.mat');
+
+% compute the CNN output and store it in result
+result = cnn_pipeline(imrgb, filterbanks, biasvectors);
+
+% calculate the maximum discrepancy between result and expected values
+max(abs(result(:) - layerResults{18}(:)))
